bc9bf8b850f741816e64a11015db1f07c6c8784c
Imtiaj Ahmed Anik <87453627+ImtiajAhmedAnik@users.noreply.github.com>
2023-02-03 16:34:30 +0000

    change

--- index.js
+++ index.js
@@ -76,5 +76,5 @@ io.on('connection', function(socket){
     });
 });
 http.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
+    console.log('Node JS Server app is running on port', app.get('port'));
 });
